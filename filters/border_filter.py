fe9e47442d39decaa7bbc74f102d5738fc5677f7
Deimos <deimoshall@gmail.com>
2022-07-16 19:50:20 -0500

    sobel working with arrays

--- filters/border_filter.py
+++ filters/border_filter.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import cv2
 
 class ManualSobel:
 
@@ -7,7 +7,7 @@ class ManualSobel:
         self._img = img
         self._img_rows = np.size(img, 0)
         self._img_cols = np.size(img, 1)
-        self._img_copy = np.zeros(self._img.shape)
+        self._img_borders = np.zeros(self._img.shape)
         self._gx = np.array([[-1, 0, 1], [-2, 0, 2], [-1, 0, 1]])
         self._gy = np.array([[-1, -2, -1], [0, 0, 0], [1, 2, 1]])
         self._kernel_rows_index = 0
@@ -17,19 +17,28 @@ class ManualSobel:
         self._a = 0
         self._num = 0
 
-    def get_border(self):
-        while self._img_cols_index < self._img_cols - 2:
-            for i in range(self._img_rows_index, self._img_rows_index + 3):
-                for j in range(self._img_cols_index, self._img_cols_index + 3):
-                    self._a = self._gx[self._kernel_rows_index, self._kernel_cols_index] * self._img[i, j]
-                    self._num += self._a
-                    self._kernel_cols_index += 1
+    def get_borders(self):
+        while self._img_rows_index < self._img_rows - 2:
+            while self._img_cols_index < self._img_cols - 2:
+                for i in range(self._img_rows_index, self._img_rows_index + 3):
+                    for j in range(self._img_cols_index, self._img_cols_index + 3):
+                        self._a = self._gx[self._kernel_rows_index, self._kernel_cols_index] * self._img[i, j]
+                        self._num += self._a
+                        self._kernel_cols_index += 1
+                    self._kernel_cols_index = 0
+                    self._kernel_rows_index += 1
                 self._kernel_cols_index = 0
-                self._kernel_rows_index += 1
-            self._kernel_cols_index = 0
-            self._kernel_rows_index = 0
-            self._img_copy[self._img_rows_index + 1, self._img_cols_index + 1] = self._num
-            self._img_cols_index += 1
+                self._kernel_rows_index = 0
+                self._img_borders[self._img_rows_index + 1, self._img_cols_index + 1] = self._num
+                self._img_cols_index += 1
+                self._num = 0
+            self._img_cols_index = 0  # This resets the x-axis for a new convolution in a new row
+            self._img_rows_index += 1
             self._num = 0
 
-        return self._img_copy
+        return self._img_borders
+
+    def show_img(self):
+        cv2.imshow("image", self._img)
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
--- main.py
+++ main.py
@@ -1,13 +1,24 @@
 import numpy as np
 from filters import border_filter
+import cv2
 
-img = np.array([[3, 0, 1, 2, 7, 4],
+img2 = np.array([[3, 0, 1, 2, 7, 4],
                 [1, 5, 8, 9, 3, 1],
                 [2, 7, 2, 5, 1, 3],
                 [0, 1, 3, 1, 7, 8],
                 [4, 2, 1, 6, 2, 8],
                 [2, 4, 5, 2, 3, 9]])
 
-sobel1 = border_filter.ManualSobel(img)
-sobel1.get_border()
-print(sobel1.get_border())
+path = "2022-07-08_12-42.png"
+img = cv2.imread(path)
+img = np.asarray(img)
+print(img.shape)
+gray_img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+print(gray_img.shape)
+print(gray_img)
+
+image_1 = border_filter.ManualSobel(img)
+image_1.show_img()
+sobel_image = image_1.get_borders()
+print(sobel_image)
+print('Program finished')
