71782951f911bf76110a9a9099d45ed8c20a25cc
Deimos <deimoshall@gmail.com>
2022-07-31 21:25:58 -0500

    Crop image working

--- filters/OpenCV.py
+++ filters/OpenCV.py
@@ -8,10 +8,13 @@ import numpy as np
 class Filter:
 
     def __init__(self, path):
+        # Image properties
         self._path = path
         self._img = cv.imread(self._path, cv.COLOR_BGR2RGB)
         self._img_width = self._img.shape[1]
         self._img_height = self._img.shape[0]
+        self._top_left_edge = [0, 0]
+        self._bottom_right_edge = [0, 0]
         self._gray_img = cv.cvtColor(self._img, cv.COLOR_BGR2GRAY)
         # Sobel parameters
         self._scale = 1
@@ -24,9 +27,10 @@ class Filter:
                                 borderType=cv.BORDER_DEFAULT)
         self._abs_grad_x = cv.convertScaleAbs(self._grad_x)
         self._abs_grad_y = cv.convertScaleAbs(self._grad_y)
+        # _grad is the average of _abs_grad_x and _abs_grad_y, contains the sobel image
         self._grad = cv.addWeighted(self._abs_grad_x, 0.5, self._abs_grad_y, 0.5, 0)
         # Threshold
-        self._thresh = cv.threshold(self._grad, 20, 255, cv.THRESH_BINARY_INV)[1]
+        self._thresh = cv.threshold(self._grad, 40, 255, cv.THRESH_BINARY_INV)[1]
         self._contours = cv.findContours(self._thresh, cv.RETR_LIST, cv.CHAIN_APPROX_SIMPLE)
         self._canvas = np.zeros_like(self._img)
 
@@ -54,22 +58,41 @@ class Filter:
         cv.imshow("Canvas", self._canvas)
         cv.waitKey(0)
 
-    def get_top_left_border(self):
-        img_rows_index = 0
-        img_cols_index = 0
+    def calculate_borders(self):
+        self._top_left_edge = [self._img_width, self._img_height]
+        self._bottom_right_edge = [0, 0]
         counter = 0
-        return self._grad[49][132]
+        x_limit = 20
+        for y in range(0, self._img_height):
+            for x in range(0, self._img_width):
+                if self._thresh[y, x] == 0:  # Means found a black pixel
+                    if counter == x_limit:
+                        # print(f"Black found at {x-x_limit},{y} counter = {counter}")
+                        # Calculate top_left_edge
+                        if y < self._top_left_edge[1]:
+                            self._top_left_edge[1] = y
+                        if x - x_limit < self._top_left_edge[0]:
+                            self._top_left_edge[0] = x - x_limit
+                        # print(f"edge: {top_left_edge[0]},{top_left_edge[1]}")
+                        # Calculate bottom_right_edge
+                        if y > self._bottom_right_edge[1]:
+                            self._bottom_right_edge[1] = y
+                        if x > self._bottom_right_edge[0]:
+                            self._bottom_right_edge[0] = x
+                    counter += 1
+                else:
+                    counter = 0
+            x = 0
+        print(f"Top edge: {self._top_left_edge[0]},{self._top_left_edge[1]}")
+        print(f"Bottom edge: {self._bottom_right_edge[0]},{self._bottom_right_edge[1]}")
+        return self._thresh[140, 60]
 
-    def cut_image(self):
-        # 49, 132 - 308, 171
-        x1 = 49
-        y1 = 132
-        x2 = 308
-        y2 = 171
-        y = 0
-        x = 0
-        h = 100
-        w = 200
-        crop = self._img[y1:y1 + 50, x1:x1 + 250]
+    def show_cropped_image(self):
+        # 56, 141 - 321, 183
+        x1 = self._top_left_edge[0]
+        y1 = self._top_left_edge[1]
+        x2 = self._bottom_right_edge[0]
+        y2 = self._bottom_right_edge[1]
+        crop = self._img[y1:y2, x1:x2]
         cv.imshow('Image', crop)
         cv.waitKey(0)
